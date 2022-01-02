6932be6091c44ade3f35646477af8c08b046fe92
MaciejBanaś <maciejb.98@gmail.com>
2022-01-02 21:19:22 +0100

    Initial commit with Spring, Maven and other dependencies

---
+++ src/main/java/pl/edu/agh/mcc/MccApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/agh/mcc/MccApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.mcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
